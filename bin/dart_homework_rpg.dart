5c54eb8d314dab417e021ad0ac9606f0553625e3
moong414 <moongom414@gmail.com>
2025-06-30 16:08:30 +0900

    first commit

---
+++ bin/dart_homework_rpg.dart
@@ -0,0 +1,5 @@
+import 'package:dart_homework_rpg/dart_homework_rpg.dart' as dart_homework_rpg;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_homework_rpg.calculate()}!');
+}
---
+++ test/dart_homework_rpg_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_homework_rpg/dart_homework_rpg.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
